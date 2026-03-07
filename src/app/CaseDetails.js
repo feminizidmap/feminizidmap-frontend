8a38c6bcd83a4e088f625c288c289aa289bd06ca
lislis <mail@lislis.de>
2026-03-07 19:22:44 +0100

    victim name

--- src/app/CaseDetails.js
+++ src/app/CaseDetails.js
@@ -1,8 +1,4 @@
 export default function CaseDetails({ props }) {
-
-
-    console.log(props);
-    
     const crime = Array.isArray(props.crime) ? props.crime[0] : props.crime;
     const victim = Array.isArray(props.victims) ? props.victims[0] : props.victims;
     const perpetrator = Array.isArray(props.perpetrators) ? props.perpetrators[0] : props.perpetrators;
@@ -10,21 +6,22 @@ export default function CaseDetails({ props }) {
     const city = props?.crime.crime_city || "Unbekannt";
     const subtitleParts = [props?.crime_date, city].filter(Boolean);
 
-    const victimName = (victim?.firstname || victim?.lastname) ? `${victim?.firstname} ${victim?.lastname?.slice(0,1)}.` : "Unbekannt";
-    const perpRelation = victim?.relationship_perpetrator_details ? `${victim?.relationship_perpetrator_details} des Opfers` : "Fremder"
+    const perpRelation = victim?.relationship_perpetrator.startsWith("Sonstig") ? `${victim?.relationship_perpetrator_details} des Opfers` : `${victim?.relationship_perpetrator} des Opfers`;
 
+    const feminicideType = victim?.type_of_feminicide.startsWith("Sonstig") ? victim?.type_of_feminicide_details : victim?.type_of_feminicide;
+  
   return (
     <article className="case-card">
       <p className="case-card-meta">{subtitleParts.join(" · ")}</p>
       <p className="case-card-description">
-      Opfer: {victimName} ({victim?.age || "Unbekannt"})
+      Opfer: {victim?.name} ({victim?.age || "Unbekannt"})
       </p>
 
       <details className="case-details">
         <summary>Mehr Details</summary>
       <div className="case-details-content">
       <p className="case-details-row">
-      Art des Femi(ni)zids: <b>{victim?.type_of_feminicide || "-"}</b>
+      Art des Femi(ni)zids: <b>{feminicideType}</b>
       </p>
       <p className="case-details-row">
       Täter: {perpRelation} ({perpetrator?.age}) 
