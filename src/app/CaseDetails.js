9373c4dc4c3d73d4d05e19729441c47b1c69b207
lislis <mail@lislis.de>
2026-03-07 18:37:20 +0100

    update map

--- src/app/CaseDetails.js
+++ src/app/CaseDetails.js
@@ -1,17 +1,21 @@
 export default function CaseDetails({ props }) {
+
+
+    console.log(props);
+    
     const crime = Array.isArray(props.crime) ? props.crime[0] : props.crime;
-    const victim = Array.isArray(props.victim) ? props.victim[0] : props.victim;
-    const perpetrator = Array.isArray(props.perpetrator) ? props.perpetrator[0] : props.perpetrator;
+    const victim = Array.isArray(props.victims) ? props.victims[0] : props.victims;
+    const perpetrator = Array.isArray(props.perpetrators) ? props.perpetrators[0] : props.perpetrators;
     const sources = Array.isArray(props.source) ? props.source : [];
-    const city = props?.crime.crime_geolocation_city || props?.victim.victim_geolocation_city || "Unbekannt";
+    const city = props?.crime.crime_city || "Unbekannt";
     const subtitleParts = [props?.crime_date, city].filter(Boolean);
 
     const victimName = (victim?.firstname || victim?.lastname) ? `${victim?.firstname} ${victim?.lastname?.slice(0,1)}.` : "Unbekannt";
-    const perpRelation = victim.relationship_perpetrator_details ? `${victim.relationship_perpetrator_details} des Opfers` : "Fremder"
+    const perpRelation = victim?.relationship_perpetrator_details ? `${victim?.relationship_perpetrator_details} des Opfers` : "Fremder"
 
   return (
     <article className="case-card">
-      <p className="case-card-meta">{subtitleParts.join(" · ")} {crime.attempt}</p>
+      <p className="case-card-meta">{subtitleParts.join(" · ")}</p>
       <p className="case-card-description">
       Opfer: {victimName} ({victim?.age || "Unbekannt"})
       </p>
@@ -20,19 +24,11 @@ export default function CaseDetails({ props }) {
         <summary>Mehr Details</summary>
       <div className="case-details-content">
       <p className="case-details-row">
+      Art des Femi(ni)zids: <b>{victim?.type_of_feminicide || "-"}</b>
+      </p>
       <p className="case-details-row">
-      Typ: <b>{crime?.type_of_feminicide?.label || "-"}</b>
+      Täter: {perpRelation} ({perpetrator?.age}) 
       </p>
-      Täter: {perpRelation} ({perpetrator.age}) 
-      </p>
-          <p className="case-details-row">
-            Todesursache: <b>{crime?.cause_of_death?.label || "-"}</b>
-          </p>
-          
-          <p className="case-details-row">
-            Tatwaffe: <b>{crime?.weapon_details || "-"}</b>
-          </p>
-         
         </div>
       </details>
     </article>
--- src/app/map/page.js
+++ src/app/map/page.js
@@ -1,13 +1,14 @@
 import Map from "./map";
 import "./page.css";
-import { getCases } from "../../services/api";
+import { getCasesPublic, getCases } from "../../services/api";
 
 async function getData() {
-  return getCases({ populate: "*", pageSize: 20000 });
+  return getCasesPublic();
 }
 
 export default async function Page() {
   const data = await getData();
+
   return (
     <main>
       <Map cases={data.data} />
